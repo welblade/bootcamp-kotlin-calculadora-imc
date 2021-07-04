1c5eab8dc16891b24a7727a95880b0b639905d72
Wellington Oliveira dos Santos <welblade@gmail.com>
2021-07-04 15:18:16 -0300

    First commit; Criação do Projeto e Definição do leiaute usando linearLayout

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 30
+    buildToolsVersion = "30.0.3"
+
+    defaultConfig {
+        applicationId = "com.github.welblade.calculadoraimc"
+        minSdk = 21
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    testImplementation("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
+    // (Required) Writing and executing Unit Tests on the JUnit Platform
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+
+    // (Optional) If you need "Parameterized Tests"
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
+
+    // (Optional) If you also have JUnit 4-based tests
+    testImplementation("junit:junit:4.13")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.7.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
---
+++ app/src/main/java/com/github/welblade/calculadoraimc/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.github.welblade.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.w("LifeCycle", "CREATE  - Estou criando a tela.")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("LifeCycle", "START   - Deixei a tela visível para você.")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.w("LifeCycle", "RESUME  - Agora você pode interagir com a tela.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("LifeCycle", "PAUSE   - A tela perdeu o foco, você não pode mais interagir.")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.w("LifeCycle", "STOP    - A tela não está mais visível, mas ainda existe.")
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        Log.w("LifeCycle", "RESTART - Não a banda, é a tela que está retomando o foco.")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.e("LifeCycle", "DESTROY - Oh não! A tela foi destruída.")
+    }
+}
